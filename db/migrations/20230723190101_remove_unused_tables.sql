fa7535f667bee35d65394917e859f4291d424093
yujin <yujinjcho@gmail.com>
2023-07-23 12:02:06 -0700

    remove old event tables

---
+++ db/migrations/20230723190101_remove_unused_tables.sql
@@ -0,0 +1,7 @@
+-- migrate:up
+DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS event_updates;
+DROP TABLE IF EXISTS events;
+
+-- migrate:down
+
--- db/schema.sql
+++ db/schema.sql
@@ -14,31 +14,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: event_updates; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.event_updates (
-    event_update_id uuid NOT NULL,
-    event_id uuid,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    properties jsonb
-);
-
-
---
--- Name: events; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.events (
-    event_id uuid NOT NULL,
-    name text,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    user_id uuid NOT NULL,
-    properties jsonb
-);
-
-
---
 -- Name: events_v2; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -66,31 +41,6 @@ CREATE TABLE public.schema_migrations (
 
 
 --
--- Name: test; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.test (
-    id text
-);
-
-
---
--- Name: event_updates event_updates_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.event_updates
-    ADD CONSTRAINT event_updates_pkey PRIMARY KEY (event_update_id);
-
-
---
--- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.events
-    ADD CONSTRAINT events_pkey PRIMARY KEY (event_id);
-
-
---
 -- Name: events_v2 events_v2_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -114,14 +64,6 @@ CREATE INDEX events_v2_project_id_created_at_idx ON public.events_v2 USING btree
 
 
 --
--- Name: event_updates event_updates_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.event_updates
-    ADD CONSTRAINT event_updates_event_id_fkey FOREIGN KEY (event_id) REFERENCES public.events(event_id);
-
-
---
 -- PostgreSQL database dump complete
 --
 
@@ -134,4 +76,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20230716040008'),
     ('20230716051811'),
     ('20230719021756'),
-    ('20230719022703');
+    ('20230719022703'),
+    ('20230723190101');
