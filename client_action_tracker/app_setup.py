15b6ee611e860ace19c23da45748d233b750f9f1
yujin <yujinjcho@gmail.com>
2023-07-24 20:40:27 -0700

    Revert "try logging to stdout"
    This reverts commit 8b309c5075deb078d0a16d5694404971cf6fe5f1.

--- client_action_tracker/app_setup.py
+++ client_action_tracker/app_setup.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 from typing import Tuple
 
 import psycopg2
@@ -15,8 +14,7 @@ from client_action_tracker.utils import get_project_id
 def create_app() -> Tuple[Flask, ClientTrackService]:
     logging.basicConfig(
         level=logging.INFO,
-        format=f'%(asctime)s - %(filename)s - %(message)s',
-        handlers=[logging.StreamHandler(sys.stdout)]
+        format=f'%(asctime)s - %(filename)s - %(message)s'
     )
 
     app = Flask(__name__)
